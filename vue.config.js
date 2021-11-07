8566222b47e07aa4b820d485d40c1e84ddcbdf7d
aboynejames <james@aboynejames.co.uk>
2021-11-07 15:02:45 +0000

    cloud prep

--- vue.config.js
+++ vue.config.js
@@ -4,13 +4,13 @@ module.exports = {
   devServer: {
     open: process.platform === 'darwin',
     host: '127.0.0.1',
-    port: 8080, // CHANGE YOUR PORT HERE!
+    port: 8081, // CHANGE YOUR PORT HERE!
     https: true,
     hotOnly: false
   },
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/'
-    : '/',
+    ? ''
+    : '',
   pluginOptions: {
     i18n: {
       locale: 'en',
