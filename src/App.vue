f44bc45e02d7e053fea886af63c220f1712fccf0
aboynejames <james@aboynejames.co.uk>
2022-10-02 10:41:15 +0100

    top nav layout changes

--- src/App.vue
+++ src/App.vue
@@ -14,27 +14,27 @@
       <div id="peer-pages">
         <router-link class="nav-item-route" to="/">{{ $t('home') }}</router-link>
         |
-        <router-link class="nav-item-route" to="/about">{{ $t('about') }}</router-link>
+        <router-link v-if="authConnectStatus !== true" class="nav-item-route" to="/about">{{ $t('about') }}</router-link>
       </div>
       <div id="peer-settings">
         <div class="toolkit-settings">
           <div class="cale-space">
-            <div class="cale-name">AI-agent:</div>
+            <div class="cale-name">AI: </div>
             <button class="toolbar-top"  v-bind:class="{ active: caleAIStatus.active === true}" type="button" @click="caleAIset">
               {{ caleAIStatus.text }}
             </button>
           </div>
         </div>
         <div class="toolkit-settings">
-          <button type="button" class="toolbar-top" id="help-live" @click="showHelpModal">
-            {{ $t('help') }}
-          </button>
-        </div>
-        <div class="toolkit-settings">
           <div class="software-status">ALPHA</div>
         </div>
         <div class="toolkit-settings">
           <button type="button" v-bind:class="{ networklive: connectBut.active === true && authConnectStatus === true}" class="connect-network" @click="connectNetwork(connectBut)">{{ connectBut.text }}</button>
+        </div>
+        <div class="toolkit-settings">
+          <button type="button" class="toolbar-top" id="help-live" @click="showHelpModal">
+            {{ $t('help') }}
+          </button>
         </div>
       </div>
     </div>
